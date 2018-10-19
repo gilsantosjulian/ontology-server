6dca0ca87f7b01ee3b26a33a209e35a1474a5496
Julian Gil <gilsantosjulian@gmail.com>
2018-10-19 09:47:19 -0500

    Create node server - serve static files (ontologies in XML, RDF and OWL format)

---
+++ routes/ontology.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET ontologies listing. */
+router.get('/', function(req, res, next) {
+    res.sendFile(path.join(__dirname, '../public/ontologies', 'html-ontology.xml'));
+});
+
+module.exports = router;
